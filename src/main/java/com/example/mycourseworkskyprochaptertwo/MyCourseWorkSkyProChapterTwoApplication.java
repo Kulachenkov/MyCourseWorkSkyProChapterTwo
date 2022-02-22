5cb24afb5307a0183748e70146cbe0f2f0e105f9
Kulachenkov <nik.k2710@gmail.com>
2022-02-22 20:35:52 +0300

    Initial commit

---
+++ src/main/java/com/example/mycourseworkskyprochaptertwo/MyCourseWorkSkyProChapterTwoApplication.java
@@ -0,0 +1,13 @@
+package com.example.mycourseworkskyprochaptertwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCourseWorkSkyProChapterTwoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyCourseWorkSkyProChapterTwoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mycourseworkskyprochaptertwo/MyCourseWorkSkyProChapterTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mycourseworkskyprochaptertwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCourseWorkSkyProChapterTwoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
